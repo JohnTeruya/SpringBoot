027a85654fff66437f5408e3996dc396d5f7ce75
johnteruya <jteruya9@gmail.com>
2018-10-21 13:52:27 -0300

    projeto inicial

---
+++ src/main/java/br/com/doacoesapp/DoacoesappApplication.java
@@ -0,0 +1,12 @@
+package br.com.doacoesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoacoesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoacoesappApplication.class, args);
+	}
+}
